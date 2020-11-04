c0a7eb95125ebc278e5b7b26421137c7f917358f
JHuffaker <73908331+JHuffaker@users.noreply.github.com>
2020-11-03 19:51:47 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 #This is a test for our repository.  Print your name and commit it git hub to see if it worked.
 print('Team Members:')
-print('Alec Swainston')
+print('Alec Swainston, Jordan Huffaker')
