630ec37c4f73956c78bb882ee32d21d2b1450cb4
alecs396 <51383806+alecs396@users.noreply.github.com>
2020-11-11 10:00:57 -0700

    Added player movement.

--- lunarlanderAlpha.py
+++ lunarlanderAlpha.py
@@ -28,9 +28,13 @@ class MyGame(arcade.Window):
         # go into a list.
 
         self.player_list = None
+        self.wall_list = None
 
         # Separate variable that holds the player sprite
         self.player_sprite = None
+
+        # Our physics engine
+        self.physics_engine = None
 
         arcade.set_background_color(arcade.csscolor.BLACK)
 
@@ -38,7 +42,8 @@ class MyGame(arcade.Window):
         """ Set up the game here. Call this function to restart the game. """
         #Create the Sprite lists (add the floor later)
         self.player_list = arcade.SpriteList()
-        self.landing_zone_list = arcade.SpriteList(use_spacial_hash=True)
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        #self.landing_zone_list = arcade.SpriteList(use_spacial_hash=True)
 
         # Set up the lander, specifically placing it at these coordinates (centered for now).
         image_source = "landerAlpha.png"
@@ -46,6 +51,9 @@ class MyGame(arcade.Window):
         self.player_sprite.center_x = 500
         self.player_sprite.center_y = 325
         self.player_list.append(self.player_sprite)
+
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
 
     def on_draw(self):
         """ Render the screen. """
@@ -56,8 +64,39 @@ class MyGame(arcade.Window):
         #Draw Sprites (add floor later)
         self.player_list.draw()
 
-class physicsEngine:
-    def __init__(self.player_sprite, self.landing_zone_list):
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
+
+#class physicsEngine:
+    #def __init__(self.player_sprite, self.landing_zone_list):
+
         
 
 
