5838b4ea30844840d78d024a85cbea04f9d700cf
alecs396 <51383806+alecs396@users.noreply.github.com>
2020-11-12 16:23:30 -0700

    PyMunk Rework
    So the physics engine needed to be reworked.  Re-wrote a bit of code to test physics engine and gravity.  Temporarily removed player input as well

--- lunarlanderAlpha.py
+++ lunarlanderAlpha.py
@@ -2,104 +2,156 @@
 Title: Lunar Lander Game
 Authors: Alec Swainston, Nathan Page, Jordan Huffaker, Samuel Omondi
 """
+import math
+from typing import Optional
 import arcade
 
-# Constants
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Lunar Lander"
 
-# Constants used to scale our sprites from their original size
-CHARACTER_SCALING = 0.5
-PLAYER_MOVEMENT_SPEED = 1
+# How big are our image tiles?
+SPRITE_IMAGE_SIZE = 64
 
+# Scale sprites up or down
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_TILES = 0.5
 
-class MyGame(arcade.Window):
-    """
-    Main application class.
-    """
+# Scaled sprite size for tiles
+SPRITE_SIZE = int(SPRITE_IMAGE_SIZE * SPRITE_SCALING_PLAYER)
 
-    def __init__(self):
+# Size of grid to show on screen, in number of tiles
+SCREEN_GRID_WIDTH = 25
+SCREEN_GRID_HEIGHT = 20
 
-        # Call the parent class and set up the window
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+# Size of screen to show, in pixels
+SCREEN_WIDTH = SPRITE_SIZE * SCREEN_GRID_WIDTH
+SCREEN_HEIGHT = SPRITE_SIZE * SCREEN_GRID_HEIGHT
 
-        # These are 'lists' that keep track of our sprites. Each sprite should
-        # go into a list.
+# --- Physics forces. Higher number, faster accelerating.
 
-        self.player_list = None
-        self.landing_zone_list = None
+# Gravity
+GRAVITY = 10
 
-        # Separate variable that holds the player sprite
-        self.player_sprite = None
+# Damping - Amount of speed lost per second
+DEFAULT_DAMPING = 1.0
+PLAYER_DAMPING = 0.4
 
-        # Our physics engine
-        self.physics_engine = None
+# Friction between objects
+PLAYER_FRICTION = 1.0
+WALL_FRICTION = 0.7
 
-        arcade.set_background_color(arcade.csscolor.BLACK)
+# Mass (defaults to 1)
+PLAYER_MASS = 2.0
+
+# Keep player from going too fast
+PLAYER_MAX_HORIZONTAL_SPEED = 450
+PLAYER_MAX_VERTICAL_SPEED = 1600
+
+class GameWindow(arcade.Window):
+    """ Main Window """
+
+    def __init__(self, width, height, title):
+        """ Create the variables """
+
+        # Init the parent class
+        super().__init__(width, height, title)
+
+        # Player sprite
+        self.player_sprite: Optional[arcade.Sprite] = None
+        
+        # Sprite lists we need
+        self.player_list: Optional[arcade.SpriteList] = None
+        self.wall_list: Optional[arcade.SpriteList] = None
+
+        # Track the current state of what key is pressed
+        self.left_pressed: bool = False
+        self.right_pressed: bool = False
+
+        # Set background color
+        arcade.set_background_color(arcade.color.BLACK)
+
+         # Physics engine
+        self.physics_engine = Optional[arcade.PymunkPhysicsEngine]
 
     def setup(self):
-        """ Set up the game here. Call this function to restart the game. """
-        #Create the Sprite lists (add the floor later)
+        """ Set up everything with the game """
+        
+        # Create the sprite lists
         self.player_list = arcade.SpriteList()
-        self.landing_zone_list = arcade.SpriteList(use_spatial_hash=True)
 
-        # Set up the lander, specifically placing it at these coordinates (centered for now).
-        image_source = "landerAlpha.png"
-        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
-        self.player_sprite.center_x = 500
-        self.player_sprite.center_y = 325
+
+        # Create player sprite
+        self.player_sprite = arcade.Sprite("landerAlpha.png",SPRITE_SCALING_PLAYER)
+
+        # Set player location
+        grid_x = 5
+        grid_y = 10
+        self.player_sprite.center_x = SPRITE_SIZE * grid_x + SPRITE_SIZE / 2
+        self.player_sprite.center_y = SPRITE_SIZE * grid_y + SPRITE_SIZE / 2
+        # Add to player sprite list
         self.player_list.append(self.player_sprite)
 
-        # Create the 'physics engine'
-        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.landing_zone_list)
+        # --- Pymunk Physics Engine Setup ---
 
-    def on_draw(self):
-        """ Render the screen. """
+        # The default damping for every object controls the percent of velocity
+        # the object will keep each second. A value of 1.0 is no speed loss,
+        # 0.9 is 10% per second, 0.1 is 90% per second.
+        # For top-down games, this is basically the friction for moving objects.
+        # For platformers with gravity, this should probably be set to 1.0.
+        # Default value is 1.0 if not specified.
+        damping = DEFAULT_DAMPING
 
-        #Clear the screen to the background color
-        arcade.start_render()
-        
-        #Draw Sprites (add floor later)
-        self.player_list.draw()
+        # Set the gravity. (0, 0) is good for outer space and top-down.
+        gravity = (0, -GRAVITY)
+
+        # Create the physics engine
+        self.physics_engine = arcade.PymunkPhysicsEngine(damping=damping,
+                                                         gravity=gravity)
+
+        # Add the player.
+        # For the player, we set the damping to a lower value, which increases
+        # the damping rate. This prevents the character from traveling too far
+        # after the player lets off the movement keys.
+        # Setting the moment to PymunkPhysicsEngine.MOMENT_INF prevents it from
+        # rotating.
+        # Friction normally goes between 0 (no friction) and 1.0 (high friction)
+        # Friction is between two objects in contact. It is important to remember
+        # in top-down games that friction moving along the 'floor' is controlled
+        # by damping.
+        self.physics_engine.add_sprite(self.player_sprite,
+                                       friction=PLAYER_FRICTION,
+                                       mass=PLAYER_MASS,
+                                       moment=arcade.PymunkPhysicsEngine.MOMENT_INF,
+                                       collision_type="player",
+                                       max_horizontal_velocity=PLAYER_MAX_HORIZONTAL_SPEED,
+                                       max_vertical_velocity=PLAYER_MAX_VERTICAL_SPEED)
+
+
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
-
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+        pass
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key. """
-
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = 0
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = 0
+        pass
 
     def on_update(self, delta_time):
         """ Movement and game logic """
+        """ Movement and game logic """
+        self.physics_engine.step()
 
-        # Move the player with the physics engine
-        self.physics_engine.update()
-
+    def on_draw(self):
+        """ Draw everything """
+        arcade.start_render()
+        self.player_list.draw()
 
 def main():
     """ Main method """
-    window = MyGame()
+    window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
     arcade.run()
 
 
 if __name__ == "__main__":
+    main()
     main()
