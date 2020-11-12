ad8c9778dfadfb10ca1b54d7fc7c03d24bf7c18c
Alec Swainston <51383806+alecs396@users.noreply.github.com>
2020-11-12 08:34:13 -0700

    Delete test.py
    This is not needed anymore

--- test.py
+++
@@ -1,8 +0,0 @@
-#This is a test for our repository.  Print your name and commit it git hub to see if it worked.
-print('Team Members:')
-<<<<<<< HEAD
-print('Alec Swainston, Jordan Huffaker')
-=======
-print('Alec Swainston, Jordan Huffaker, Nathan Page')
-print("Samuel ")
->>>>>>> dfa8666ea8304e44562b74de1d288ae28fa1b52b
