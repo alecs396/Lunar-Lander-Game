c412b81f9285ba012b461f956767729be00acf0b
alecs396 <51383806+alecs396@users.noreply.github.com>
2020-12-01 12:45:09 -0700

    Amazing Overhaul by brother manley

--- lundarLanderBeta.py
+++ lundarLanderBeta.py
@@ -5,6 +5,7 @@ Authors: Alec Swainston, Nathan Page, Jordan Huffaker, Samuel Omondi
 import math
 from typing import Optional
 import arcade
+import os
 
 SCREEN_TITLE = "Lunar Lander"
 
@@ -49,61 +50,27 @@ PLAYER_MAX_VERTICAL_SPEED = 1600
 #Player Movement Force
 PLAYER_MOVE_FORCE = 65
 
-
 class GameWindow(arcade.Window):
-    """ Main Window 
-        Stereotype: Controller
-    """
+    """ Main Window """
 
     def __init__(self, width, height, title):
         """ Create the variables """
-        
-        # Create Instances of other Classes
-        self.lander = Lander()
+
         # Init the parent class
         super().__init__(width, height, title)
-
-        # Player sprite
-        self.player_sprite: Optional[arcade.Sprite] = None
-        
-        # Sprite lists we need
-        self.player_list: Optional[arcade.SpriteList] = None
-        self.wall_list: Optional[arcade.SpriteList] = None
-
-        # Track the current state of what key is pressed
-        self.left_pressed: bool = False
-        self.right_pressed: bool = False
-        self.up_pressed: bool = False
-
-        # Set background color
-        arcade.set_background_color(arcade.color.BLACK)
 
          # Physics engine
         self.physics_engine = Optional[arcade.PymunkPhysicsEngine]
 
+        # MM: Lander object
+        self.lander = Lander()
+        self.output_service = OutputService()
+        self.input_service = InputService()
+
     def setup(self):
         """ Set up everything with the game """
-        
-        # Create the sprite lists
-        self.player_list = arcade.SpriteList()
 
-         # Read in the tiled map
-        map_name = "pymunk_test_map.tmx"
-        my_map = arcade.tilemap.read_tmx(map_name)
-
-        # Read in the map layers
-        self.wall_list = arcade.tilemap.process_layer(my_map, 'Tile Layer 1', SPRITE_SCALING_TILES)
-
-        # Create player sprite
-        self.player_sprite = arcade.Sprite("landerAlpha.png",SPRITE_SCALING_PLAYER)
-
-        # Set player location
-        grid_x = 12
-        grid_y = 15
-        self.player_sprite.center_x = SPRITE_SIZE * grid_x + SPRITE_SIZE / 2
-        self.player_sprite.center_y = SPRITE_SIZE * grid_y + SPRITE_SIZE / 2
-        # Add to player sprite list
-        self.player_list.append(self.player_sprite)
+        self.output_service.setup()
 
         # --- Pymunk Physics Engine Setup ---
 
@@ -132,13 +99,15 @@ class GameWindow(arcade.Window):
         # Friction is between two objects in contact. It is important to remember
         # in top-down games that friction moving along the 'floor' is controlled
         # by damping.
-        self.physics_engine.add_sprite(self.player_sprite,
+        self.physics_engine.add_sprite(self.output_service.player_sprite,
                                        friction=PLAYER_FRICTION,
                                        mass=PLAYER_MASS,
                                        moment=arcade.PymunkPhysicsEngine.MOMENT_INF,
                                        collision_type="player",
                                        max_horizontal_velocity=PLAYER_MAX_HORIZONTAL_SPEED,
                                        max_vertical_velocity=PLAYER_MAX_VERTICAL_SPEED)
+
+        #self.physics_service.add_sprite(self.output_service.player_sprite)
 
         # Create the walls.
         # By setting the body type to PymunkPhysicsEngine.STATIC the walls can't
@@ -147,48 +116,45 @@ class GameWindow(arcade.Window):
         # PymunkPhysicsEngine.KINEMATIC objects will move, but are assumed to be
         # repositioned by code and don't respond to physics forces.
         # Dynamic is default.
-        self.physics_engine.add_sprite_list(self.wall_list,
+        self.physics_engine.add_sprite_list(self.output_service.wall_list,
                                             friction=WALL_FRICTION,
                                             collision_type="wall",
                                             body_type=arcade.PymunkPhysicsEngine.STATIC)     
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
+        self.input_service.apply_input(key, self.lander)
         
-        self.lander.move(key, modifiers)
-        
-
-    def on_key_release(self, key, modifiers):
-        """Called when the user releases a key. """
-        
-        self.lander.notMove(key, modifiers)
-        
-
+   
     def on_update(self, delta_time):
         """ Movement and game logic """
 
-        # Update player forces based on keys pressed
-        if self.left_pressed and not self.right_pressed:
-            # Create a force to the left. Apply it. Change this to rotation later.
-            force = (-PLAYER_MOVE_FORCE, 0)
-            self.physics_engine.apply_force(self.player_sprite, force)
-        elif self.right_pressed and not self.left_pressed:
-            # Create a force to the right. Apply it. Change this to rotation later
-            force = (PLAYER_MOVE_FORCE, 0)
-            self.physics_engine.apply_force(self.player_sprite, force)
-        elif self.up_pressed:
-            force = (0, PLAYER_MOVE_FORCE)
-            self.physics_engine.apply_force(self.player_sprite, force)
+        #self.physics_service.apply_forces(self.lander, self.output_service)
 
+        thrust = self.lander.get_thrust()
+        self.physics_engine.apply_force(self.output_service.player_sprite, thrust)
+
+        # MM: Check for collision with ground
+        # MM: Update state of lander
 
         # Moving objects in physics engine
         self.physics_engine.step()
 
     def on_draw(self):
         """ Draw everything """
+
+        # MM: Consider moving all of this out to a "DrawingService" class or 
+        # something like that. For example...
+        # self.drawing_service.draw_walls(wall_list)
+        # self.drawing_service.draw_lander(self.player_sprite)
+        # self.drawing_service.draw_explosions()
+        
         arcade.start_render()
-        self.wall_list.draw()
-        self.player_list.draw()
+        self.output_service.wall_list.draw()
+
+        self.output_service.draw_lander(self.lander)
+
+        
 
 class Lander:
     """The Ship the Player controls
@@ -197,32 +163,92 @@ class Lander:
             (Consider making a class to house the movement methods)
     """
     def __init__(self):
-        pass
-    
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.LEFT:
-            self.left_pressed = True
-        elif key == arcade.key.RIGHT:
-            self.right_pressed = True
-        elif key == arcade.key.UP:
-            self.up_pressed = True
-    
-    def on_key_release(self, key, modifiers):
-        if key == arcade.key.LEFT:
-            self.left_pressed = False
-        elif key == arcade.key.RIGHT:
-            self.right_pressed = False
-        elif key == arcade.key.UP:
-            self.up_pressed = False
-    
-class Landscape():
-    """This is the landscape of the map.  The landscape should be able to collide and interact with the lander. In otherwords this is the ground.
-        Stereotype:
-            Undecided?
-    """
-    pass
+        self._thrust = (0, 0)
+        self._rotation = 0.0
+        self._fuel = 2000
+        self._has_crashed = False
 
-#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+    def get_fuel(self):
+        return self._fuel
+
+    def get_rotation(self):
+        return self._rotation
+
+    def get_thrust(self):
+        if self._fuel == 0:
+            return (0, 0)
+        else:
+            # self._fuel -= 10
+            return self._thrust
+
+    def set_rotation(self, rotation):
+        self._rotation += rotation
+        if self._rotation > 45:
+            self._rotation = 45
+ 
+    def set_thrust(self, thrust):
+        if thrust[0] > 0:
+            self._rotation += 5
+        elif thrust[0] < 0:
+            self._rotation += 5
+        self._thrust = thrust
+    
+
+class InputService:
+
+    def __init__(self):
+        pass
+
+    def apply_input(self, key, lander):
+        force = (0, 0)
+        if key == arcade.key.LEFT:
+            force = (-PLAYER_MOVE_FORCE, 0)
+        elif key == arcade.key.RIGHT:
+            force = (PLAYER_MOVE_FORCE, 0)
+        elif key == arcade.key.UP:
+            force = (0, PLAYER_MOVE_FORCE)
+        lander.set_thrust(force)
+
+class OutputService:
+    
+    def __init__(self):
+        self.player_sprite: Optional[arcade.Sprite] = None
+        self.player_list: Optional[arcade.SpriteList] = None
+        self.wall_list: Optional[arcade.SpriteList] = None
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def setup(self):
+        self.directory = os.path.dirname(__file__)
+        map_name = os.path.join(self.directory, "pymunk_test_map.tmx")
+        my_map = arcade.tilemap.read_tmx(map_name)
+        self.wall_list = arcade.tilemap.process_layer(my_map, 'Tile Layer 1', SPRITE_SCALING_TILES)
+
+        self.player_list = arcade.SpriteList()
+        
+        # Read in the map layers
+        
+        # Create player sprite
+        file_name = os.path.join(self.directory, "landerAlpha.png")
+        self.player_sprite = arcade.Sprite(file_name, SPRITE_SCALING_PLAYER)
+
+        # Set player location
+        grid_x = 12
+        grid_y = 15
+        self.player_sprite.center_x = SPRITE_SIZE * grid_x + SPRITE_SIZE / 2
+        self.player_sprite.center_y = SPRITE_SIZE * grid_y + SPRITE_SIZE / 2
+        # Add to player sprite list
+        self.player_list.append(self.player_sprite)
+
+    def draw_lander(self, lander):
+        if lander.get_thrust()[0] < 0:
+            self.player_sprite.turn_left(lander.get_rotation())
+        elif lander.get_thrust()[0] > 0:
+            self.player_sprite.turn_right(lander.get_rotation())
+        # MM: draw remaining fuel indicator
+        # MM: draw explosion if lander.hp = 0
+        self.player_list.draw()
+
+
 
 def main():
     """ Main method """
@@ -232,3 +258,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
